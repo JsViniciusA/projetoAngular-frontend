99d89afffefa448525b9fa335e72f7423b345b9b
jvat2 <jvat2@poli.br>
2024-11-06 21:32:49 +0000

    atualizacoes e adicoes de novos arquivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BuscadorComponent } from './paginas/buscador/buscador.component';
 import { ListagemComponent } from './paginas/listagem/listagem.component';
+import { AdicionadorComponent } from './paginas/adicionador/adicionador.component';
 
 // Suggested code may be subject to a license. Learn more: ~LicenseLog:3784982556.
 const routes: Routes = [
   { path: '', redirectTo: '/buscador', pathMatch: 'full' },
   { path: 'buscador', component: BuscadorComponent },
-  { path: 'listagem', component: ListagemComponent }
+  { path: 'listagem', component: ListagemComponent },
+  { path: 'adicionador', component: AdicionadorComponent}
 ];
 
 @NgModule({
